ab973fd6b3f840d447b1843e22e29ad243f0fc80
Valhe Kouneli <valhe.kouneli@cgi.com>
2023-12-01 06:56:12 +0200

    Poista input arraysta tyhjät merkkijonot

--- src/util.ts
+++ src/util.ts
@@ -5,7 +5,7 @@ export function readFromFile(fileName: string): string[] {
   try {
     const filePath = path.join(__dirname, fileName);
     const data = fs.readFileSync(filePath, "utf-8");
-    return data.split("\n");
+    return data.split("\n").filter((a) => !!a);
   } catch (err) {
     console.error(`Error reading file ${fileName}: ${(err as Error).message}`);
     return [];
