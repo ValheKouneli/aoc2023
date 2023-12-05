3365af1366a7a3c5f9051cf215511b11c9d1e918
Valhe Kouneli <valhe.kouneli@cgi.com>
2023-12-05 14:15:46 +0200

    day05 pt2

--- src/day05.ts
+++ src/day05.ts
@@ -1,3 +1,4 @@
+import { start } from "repl";
 import { readFromFile } from "./util";
 const input: string[] = readFromFile("inputs/input05.txt", true);
 
@@ -13,90 +14,110 @@ const groups = input.reduce(
   [[]] as string[][]
 );
 
+type Filter = {
+  targetbeg: number;
+  sourcebeg: number;
+  length: number;
+};
+
 const seeds = groups[0][0]
   .split(": ")[1]
   .split(" ")
   .map((a) => parseInt(a));
-const filters = groups
+const filters: Filter[][] = groups
   .slice(1, groups.length)
   .map((a) => a.slice(1, a.length))
-  .map((a) => {
+  .map((a: string[]) => {
     return a.map((a) => {
-      return a.split(" ").map((a) => parseInt(a));
+      const [targetbeg, sourcebeg, length] = a
+        .split(" ")
+        .map((a) => parseInt(a));
+      return {
+        targetbeg,
+        sourcebeg,
+        length,
+      } as Filter;
     });
-  });
-
+  })
+  .map((a: Filter[]) => a.sort((a, b) => a.sourcebeg - b.sourcebeg));
 const seedsActual = seeds.filter((seed, index) => index % 2 == 0);
 const seedsRange = seeds.filter((seed, index) => index % 2 == 1);
+const startingRanges = seedsActual.map((beginning, index) => ({
+  beg: beginning,
+  end: beginning + seedsRange[index] - 1,
+}));
 
-const filtersReversed = [...filters].reverse();
-const filtersReveredAllButLast = filtersReversed.slice(
-  1,
-  filtersReversed.length
+console.log("startingRanges", startingRanges, "\n\n");
+
+type Range = {
+  beg: number;
+  end: number;
+};
+
+console.log("filters", filters, "\n\n");
+
+const finalRanges = filters.reduce(
+  (ranges: Range[], filter: Filter[], index: number) => {
+    const rangesAfterFilter = ranges.reduce(
+      (allRanges: Range[], rangeHere: Range) => {
+        console.log("\nTHIS RANGE", rangeHere);
+        const rangesForThisRange = fromRangeGetRanges(rangeHere, filter);
+        console.log("RAGES FOR THIS RANGE", rangesForThisRange);
+        allRanges = allRanges.concat(rangesForThisRange);
+        return allRanges;
+      },
+      []
+    );
+    console.log("Ranges after filter", index, rangesAfterFilter);
+    return rangesAfterFilter;
+  },
+  startingRanges
 );
+console.log("\n\nfinalRanges", finalRanges);
+const part2 = [...finalRanges].sort((a, b) => a.beg - b.beg)[0].beg;
+console.log("part2", part2);
 
-function getSourceNumber(i: number, print?: boolean) {
-  return filtersReveredAllButLast.reduce(
-    (curNumber: number, curFilter: number[][]) => {
-      const previous = findNumberSource(curNumber, curFilter, print);
-      return previous;
-    },
-    i
-  );
-}
-
-function sourceNumberIsInSource(i: number) {
-  const found = seedsActual.some(
-    (startPos, index) => i >= startPos && i < startPos + seedsRange[index]
-  );
-  return found;
-}
-
-function findNumberSource(i: number, filter: number[][], print?: boolean) {
-  let numberSource = i;
-  filter.some((row: number[]) => {
-    if (i >= row[0] && i < row[0] + row[2]) {
-      numberSource = row[1] + (i - row[0]);
-      return true;
-    }
-  });
-  if (print) {
-    console.log("  findNumberSource");
-    console.log("  i", i);
-    console.log("  filter", filter);
-    console.log("  numberSource", numberSource);
-  }
-  return numberSource;
-}
-
-let smallestIndex = -1;
-
-const sortedLast = [...filtersReversed[0]].sort((a, b) => a[0] - b[0]);
-console.log("sortedLast", sortedLast);
-
-const lowestPossibleInTargets = sortedLast[0][0];
-if (lowestPossibleInTargets) {
-  for (let i = 0; i < lowestPossibleInTargets; i++) {
-    console.log("\nMOIKKELIS", i);
-    const found = sourceNumberIsInSource(getSourceNumber(i));
-    if (found) {
-      smallestIndex = i;
-      break;
-    }
-  }
-}
-if (smallestIndex == -1) {
-  sortedLast.some((range) => {
-    for (let i = range[0]; i < range[0] + range[2]; i++) {
-      console.log("\nMOIKKELIS", i);
-      const sourceNumber = getSourceNumber(i);
-      const found = sourceNumberIsInSource(sourceNumber);
-      if (found) {
-        smallestIndex = i;
-        return true;
+function fromRangeGetRanges(range: Range, filter: Filter[]): Range[] {
+  const ranges: Range[] = [];
+  const leftOfRange = filter.reduce((leftOfRange, currentFilter) => {
+    if (leftOfRange.beg < leftOfRange.end) {
+      console.log("  leftOfRange", leftOfRange);
+      const currentFilterLast =
+        currentFilter.sourcebeg + currentFilter.length - 1;
+      if (currentFilterLast >= range.beg) {
+        console.log("  currentFilter that will be applied", currentFilter);
+        const handledRange = {
+          beg: leftOfRange.beg,
+          end: Math.min(
+            leftOfRange.end,
+            currentFilter.sourcebeg + currentFilter.length - 1
+          ),
+        };
+        console.log("  handledRange", handledRange);
+        const rangeTroughFilter = {
+          beg:
+            currentFilter.targetbeg +
+            (handledRange.beg - currentFilter.sourcebeg),
+          end:
+            currentFilter.targetbeg +
+            (handledRange.end - currentFilter.sourcebeg),
+        };
+        console.log("  rangeAdded (YES filter)", rangeTroughFilter);
+        ranges.push(rangeTroughFilter);
+        leftOfRange = {
+          beg: handledRange.end + 1,
+          end: leftOfRange.end,
+        };
+      } else {
+        console.log("not handled");
       }
     }
-  });
+    console.log("  setLeftOfRange", leftOfRange);
+    return leftOfRange;
+  }, range);
+  if (leftOfRange.beg < leftOfRange.end) {
+    console.log("  rangeAdded (not filter)", leftOfRange);
+    ranges.push(leftOfRange);
+  }
+  return ranges;
 }
-
-console.log("smallestIndex", smallestIndex);
