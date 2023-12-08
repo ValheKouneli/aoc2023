8484d302b4a2544a73243cb975a9619003d94a84
Valhe Kouneli <valhe.kouneli@cgi.com>
2023-12-08 23:12:46 +0200

    day08

--- src/day08.ts
+++ src/day08.ts
@@ -1,4 +1,5 @@
 import { readFromFile } from "./util";
+import lcm from "compute-lcm";
 const input: string[] = readFromFile("inputs/input08.txt");
 const instructions: number[] = [...input[0]].map((i) => (i == "L" ? 0 : 1));
 const mappings: Map<string, string[]> = input
@@ -28,7 +29,6 @@ function getPart1(startingCoordinate: string) {
 
     pointer = pointer + 1 < instructions.length ? pointer + 1 : 0;
     stepsTaken += 1;
-    //console.log("cordinateNow", coordinateNow);
   }
   return stepsTaken;
 }
@@ -47,8 +47,8 @@ function getPersonalMap(startingCoordinate: string): PersonalMap {
   let pointer = 0;
   let stepsTaken = 0;
   let coordinateNow: string = startingCoordinate;
+  // break out when loop has been found
   while (!personalMap.get(coordinateNow)) {
-    // loop has been found
     const instruction = instructions[pointer];
     coordinateNow = getNextNode(coordinateNow, instruction);
     pointer = pointer + 1 < instructions.length ? pointer + 1 : 0;
@@ -62,118 +62,32 @@ function getPersonalMap(startingCoordinate: string): PersonalMap {
   return personalMap;
 }
 
-type CurrentPositionInPersonalMap = {
-  lastZ: string;
-  stepsAfterLastZ: number;
-};
+const personalMaps: PersonalMap[] = startingCoordinates.map((start) =>
+  getPersonalMap(start)
+);
+console.log("personalMaps", personalMaps);
+// prints out:
+// personalMaps [
+//   Map(2) { 'DLA' => [ 'XBZ', 20777 ], 'XBZ' => [ 'XBZ', 20777 ] },
+//   Map(2) { 'DNA' => [ 'LKZ', 19199 ], 'LKZ' => [ 'LKZ', 19199 ] },
+//   Map(2) { 'XLA' => [ 'XQZ', 17621 ], 'XQZ' => [ 'XQZ', 17621 ] },
+//   Map(2) { 'AAA' => [ 'ZZZ', 15517 ], 'ZZZ' => [ 'ZZZ', 15517 ] },
+//   Map(2) { 'JVA' => [ 'KDZ', 13939 ], 'KDZ' => [ 'KDZ', 13939 ] },
+//   Map(2) { 'SHA' => [ 'NTZ', 12361 ], 'NTZ' => [ 'NTZ', 12361 ] }
+// ]
 
-function applySteps(
-  steps: number,
-  currentPos: CurrentPositionInPersonalMap,
-  personalMap: PersonalMap
-): CurrentPositionInPersonalMap {
-  //console.log("apply", steps, "to", currentPos);
-  const value = personalMap.get(currentPos.lastZ);
-  //console.log("value", value);
-  const stepsToNext = value![1] - currentPos.stepsAfterLastZ;
-  //console.log("stepsToNext", stepsToNext);
-  if (stepsToNext <= 0) {
-    throw new Error("APUA");
-  }
-  let ret;
-  if (stepsToNext > steps) {
-    ret = {
-      lastZ: currentPos.lastZ,
-      stepsAfterLastZ: currentPos.stepsAfterLastZ + steps,
-    };
-  } else {
-    ret = {
-      lastZ: value![0],
-      stepsAfterLastZ: (steps - stepsToNext) % value![1],
-    };
-  }
-  //console.log("ret", ret);
-  return ret;
+if (
+  personalMaps.some((map) => [...map.values()][0][1] != [...map.values()][1][1])
+) {
+  throw new Error("Shit just got complucated");
+} else {
+  console.log("We are very lucky O_O");
 }
-
-function howManyStepsToNextZ(
-  currentPos: CurrentPositionInPersonalMap,
-  personalMap: PersonalMap
-): number {
-  const value = personalMap.get(currentPos.lastZ);
-  const left = value![1] - currentPos.stepsAfterLastZ;
-  return left;
-}
-
-type Place = {
-  personalMap: PersonalMap;
-  currentPos: CurrentPositionInPersonalMap;
-};
-
-const places: Place[] = startingCoordinates.map((start) => ({
-  personalMap: getPersonalMap(start),
-  currentPos: {
-    lastZ: start,
-    stepsAfterLastZ: 0,
-  },
-}));
-console.log(
-  "personalMaps",
-  places.map((p) => p.personalMap)
+const steps: number[] = personalMaps.map(
+  (map) => map.get([...map.keys()][1])![1]
 );
 
-function getBiggestAmountsOfStepsToNextZ(places: Place[]) {
-  const biggest = places
-    .map((place) => howManyStepsToNextZ(place.currentPos, place.personalMap))
-    .sort((a, b) => a - b)
-    .pop() as number;
-  //console.log("getBiggestAmountOfSteps", biggest);
+console.log("steps", steps);
 
-  return biggest;
-}
-
-function getSmallestAmountsOfStepsToNextZ(places: Place[]) {
-  const smallest = places
-    .map((place) => howManyStepsToNextZ(place.currentPos, place.personalMap))
-    .sort((a, b) => b - a)
-    .pop() as number;
-
-  return smallest;
-}
-
-function getPart2(places: Place[]) {
-  const biggestNumberOfStepsToFirstZ = getBiggestAmountsOfStepsToNextZ(places);
-  const initialPositions = places.map((place) => ({
-    personalMap: place.personalMap,
-    currentPos: applySteps(
-      biggestNumberOfStepsToFirstZ,
-      place.currentPos,
-      place.personalMap
-    ),
-  }));
-  console.log(
-    "initial position",
-    initialPositions.map((i) => i.currentPos)
-  );
-  let steps = biggestNumberOfStepsToFirstZ;
-  let positions = initialPositions;
-  while (
-    positions.filter((pos) => pos.currentPos.stepsAfterLastZ == 0).length <= 1
-  ) {
-    console.log(positions.map((i) => i.currentPos));
-    const howManyStepsToTake = getSmallestAmountsOfStepsToNextZ(positions);
-    console.log("howManyStepsToTake", howManyStepsToTake);
-    positions = positions.map((place) => ({
-      personalMap: place.personalMap,
-      currentPos: applySteps(
-        howManyStepsToTake,
-        place.currentPos,
-        place.personalMap
-      ),
-    }));
-    steps += howManyStepsToTake;
-  }
-  console.log(positions.map((i) => i.currentPos));
-  return steps;
-}
-console.log("part2", getPart2(places));
+const part2 = lcm(steps);
+console.log("part2", part2);
