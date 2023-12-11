525db5202a66ce5f7a996608e97564f82dc7d001
Valhe Kouneli <valhe.kouneli@cgi.com>
2023-12-11 12:41:10 +0200

    day11 pt1 attempt

--- src/day11.ts
+++ src/day11.ts
@@ -39,3 +39,46 @@ printUniverse(universeExpandedBothWays);
 function printUniverse(universe: string[]) {
   universe.forEach((row) => console.log(row));
 }
+
+function findGalaxies(universe: string[]) {
+  return universe.reduce(
+    (acc, cur, index) => acc.concat(findGalaxiesOfRow(cur, index)),
+    [] as number[][]
+  );
+}
+
+function findGalaxiesOfRow(row: string, rowIndex: number) {
+  let remainingString = row;
+  const galaxies: number[][] = [];
+  let offset = 0;
+  while (remainingString.match(/#/)) {
+    const col = remainingString.indexOf("#");
+    galaxies.push([rowIndex, offset + col]);
+    remainingString = remainingString.slice(col + 1, remainingString.length);
+    offset += col;
+  }
+  return galaxies;
+}
+
+const galaxies = findGalaxies(universeExpandedBothWays);
+console.log("galaxies", galaxies);
+
+function sumOfShortestDistances(galaxies: number[][]) {
+  let sum = 0;
+  for (let i = 0; i < galaxies.length - 1; i++) {
+    for (let j = i + 1; j < galaxies.length; j++) {
+      const galaxyA = galaxies[i];
+      const galaxyB = galaxies[j];
+      const distance =
+        Math.abs(galaxyA[0] - galaxyB[0]) + Math.abs(galaxyA[1] - galaxyB[1]);
+      //console.log("distance between", galaxyA, galaxyB, "is", distance);
+      sum += distance;
+    }
+  }
+  return sum;
+}
+
+const part1 = sumOfShortestDistances(galaxies);
+console.log("part1", part1);
+
+// 10043590 too low
