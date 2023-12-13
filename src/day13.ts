c58169eacebd7504105a9e1c986a77f0e517c7ac
Valhe Kouneli <valhe.kouneli@cgi.com>
2023-12-13 16:27:19 +0200

    day13 pt2 not cleaned up

--- src/day13.ts
+++ src/day13.ts
@@ -23,6 +23,26 @@ function findHorizontalMirrorPoint(image: string[]): number | null {
   return null;
 }
 
+function findHorizontalMirrorPointWithSmudge(image: string[]): number | null {
+  console.log("findHorizontalMirrorPointWithSmudge");
+  for (let i = 0 + 1; i < image.length; i++) {
+    console.log("i", i);
+    const thisRowsAboveIsCorrect = checkIfRowsAboveIsCorrectWithSmudge(
+      image,
+      i
+    );
+    if (thisRowsAboveIsCorrect) return i;
+  }
+  return null;
+}
+
+function findVerticalMirrorPointWithSmudge(image: string[]): number | null {
+  console.log("findVerticalMirrorPointWithSmudge");
+  const turnedImage = turnImage90DegClockwise(image);
+  printImage(turnedImage);
+  return findHorizontalMirrorPointWithSmudge(turnedImage);
+}
+
 function findVerticalMirrorPoint(image: string[]): number | null {
   console.log("findVerticalMirrorPoint");
   const turnedImage = turnImage90DegClockwise(image);
@@ -52,6 +72,25 @@ function checkIfRowsAboveIsCorrect(image: string[], rowsAbove: number) {
   return true;
 }
 
+function checkIfRowsAboveIsCorrectWithSmudge(
+  image: string[],
+  rowsAbove: number
+) {
+  const heightToCheck = Math.min(rowsAbove, image.length - rowsAbove);
+  let smudges = 0;
+  for (let i = 0; i < heightToCheck; i++) {
+    const a = image[rowsAbove - 1 - i];
+    const b = image[rowsAbove + i];
+    for (let j = 0; j < a.length; j++) {
+      if (a[j] != b[j]) {
+        smudges++;
+        if (smudges > 2) return false;
+      }
+    }
+  }
+  return smudges == 1;
+}
+
 function turnImage90DegClockwise(image: string[]) {
   const newImage: string[] = [];
 
@@ -73,14 +112,33 @@ function printImage(image: string[]) {
 }
 
 console.log("images", images);
-const part1 = images.reduce((sum, image) => {
+// const part1 = images.reduce((sum, image) => {
+//   //printImage(image);
+//   const horizontalMirrorPoint = findHorizontalMirrorPoint(image);
+//   if (horizontalMirrorPoint) {
+//     //console.log("horizontalMirrorPoint", horizontalMirrorPoint);
+//     return sum + 100 * horizontalMirrorPoint;
+//   } else {
+//     const verticalMirrorPoint = findVerticalMirrorPoint(image);
+//     //console.log("verticalMirrorPoint", verticalMirrorPoint);
+//     if (!verticalMirrorPoint) {
+//       printImage(image);
+//       throw new Error("No mirror point found!");
+//     }
+//     return sum + verticalMirrorPoint;
+//   }
+// }, 0);
+
+// console.log("part1", part1);
+
+const part2 = images.reduce((sum, image) => {
   //printImage(image);
-  const horizontalMirrorPoint = findHorizontalMirrorPoint(image);
+  const horizontalMirrorPoint = findHorizontalMirrorPointWithSmudge(image);
   if (horizontalMirrorPoint) {
     //console.log("horizontalMirrorPoint", horizontalMirrorPoint);
     return sum + 100 * horizontalMirrorPoint;
   } else {
-    const verticalMirrorPoint = findVerticalMirrorPoint(image);
+    const verticalMirrorPoint = findVerticalMirrorPointWithSmudge(image);
     //console.log("verticalMirrorPoint", verticalMirrorPoint);
     if (!verticalMirrorPoint) {
       printImage(image);
@@ -90,4 +148,4 @@ const part1 = images.reduce((sum, image) => {
   }
 }, 0);
 
-console.log("part1", part1);
+console.log("part2", part2);
