0b9457318880ec90ac7ec0c5aae35bd85763835c
Valhe Kouneli <valhe.kouneli@cgi.com>
2023-12-14 21:24:13 +0200

    temp

--- src/day14.ts
+++ src/day14.ts
@@ -1,32 +1,44 @@
 import { readFromFile } from "./util";
-const image: string[] = readFromFile("inputs/input14.txt");
+const platform: string[] = readFromFile("inputs/test.txt");
 
-function turnImage90DegAntiClockwise(image: string[]) {
+function turnPlatform90DegAntiClockwise(platform: string[]) {
+  const newPlatform: string[] = [];
+
+  for (let origCol = 0; origCol < platform[0].length; origCol++) {
+    const colToHandle = platform[0].length - 1 - origCol;
+    for (const element of platform) {
+      if (origCol == 0) {
+        newPlatform.push("");
+      }
+      newPlatform[origCol] += element[colToHandle];
+    }
+  }
+  return newPlatform;
+}
+
+function turnPlatform90DegClockwise(image: string[]) {
   const newImage: string[] = [];
 
-  for (let origCol = 0; origCol < image[0].length; origCol++) {
-    const colToHandle = image[0].length - 1 - origCol;
-    for (const element of image) {
-      if (origCol == 0) {
+  for (let origRowOrder = 0; origRowOrder < image.length; origRowOrder++) {
+    const rowToHandle = image[image.length - 1 - origRowOrder];
+    for (let origCol = 0; origCol < image[0].length; origCol++) {
+      if (origRowOrder == 0) {
         newImage.push("");
       }
-      newImage[origCol] += element[colToHandle];
+      newImage[origCol] += rowToHandle[origCol];
     }
   }
   return newImage;
 }
 
-const imageTurned = turnImage90DegAntiClockwise(image);
+const platformTurned = turnPlatform90DegAntiClockwise(platform);
 
-function printImage(image: string[]) {
+function printPlatform(platform: string[]) {
   console.log();
-  image.forEach((row) => console.log(row));
+  platform.forEach((row) => console.log(row));
 }
 
-printImage(image);
-printImage(imageTurned);
-
-function getLoadOfRow(row: string) {
+function getWestLoadOfRowIfTiltedWest(row: string) {
   let dropPoint = row.length;
   let sum = 0;
   for (let i = 0; i < row.length; i++) {
@@ -41,5 +53,71 @@ function getLoadOfRow(row: string) {
   return sum;
 }
 
-const part1 = imageTurned.reduce((sum, row) => sum + getLoadOfRow(row), 0);
+function tiltPlatforEast(platform: string[]) {
+  const tiltedPlatform = platform.map((row) => tiltRowEast(row));
+  return tiltedPlatform;
+}
+
+function getLoadOnNorthWithoutTilting(platform: string[]) {
+  let load = 0;
+  for (let i = 0; i < platform.length; i++) {
+    const row = platform[i];
+    const roundStones = row.match(/O/g)?.length ?? 0;
+    load += roundStones * (platform.length - i);
+  }
+  return load;
+}
+
+function tiltRowEast(row: string) {
+  const tiltedRow = row
+    .split("#")
+    .map((section) => {
+      const nbrOfRound = section.match(/O/g)?.length || 0;
+      const tiltedSection =
+        ".".repeat(section.length - nbrOfRound) + "O".repeat(nbrOfRound);
+      return tiltedSection;
+    })
+    .join("#");
+  return tiltedRow;
+}
+
+const part1 = platformTurned.reduce(
+  (sum, row) => sum + getWestLoadOfRowIfTiltedWest(row),
+  0
+);
 console.log("part1", part1);
+
+let platformNow = platform;
+for (let i = 0; i < 999 * 4; i++) {
+  platformNow = turnPlatform90DegClockwise(platformNow);
+  platformNow = tiltPlatforEast(platformNow);
+
+  const modulo = i % 4;
+  if (modulo == 0) {
+    console.log("\nTilted north", i);
+    const platformNorthSideUp = turnPlatform90DegAntiClockwise(platformNow);
+    printPlatform(platformNorthSideUp);
+    const load = getLoadOnNorthWithoutTilting(platformNorthSideUp);
+    console.log("load", load);
+  } else if (modulo == 1) {
+    console.log("\nTilted west", i);
+    const platformNorthSideUp = turnPlatform90DegAntiClockwise(
+      turnPlatform90DegAntiClockwise(platformNow)
+    );
+    printPlatform(platformNorthSideUp);
+    const load = getLoadOnNorthWithoutTilting(platformNorthSideUp);
+    console.log("load", load);
+  } else if (modulo == 2) {
+    console.log("\nTilted south", i);
+    const platformNorthSideUp = turnPlatform90DegClockwise(platformNow);
+    printPlatform(platformNorthSideUp);
+    const load = getLoadOnNorthWithoutTilting(platformNorthSideUp);
+    console.log("load", load);
+  } else if (modulo == 3) {
+    console.log("\nTilted east", i);
+    const platformNorthSideUp = platformNow;
+    printPlatform(platformNorthSideUp);
+    const load = getLoadOnNorthWithoutTilting(platformNorthSideUp);
+    console.log("load", load);
+  }
+}
